0239ea75a3ef8a3b9e6af76daa3f5b45122b9bc0
qaziyousuf701@gmail.com <qaziyousuf701@gmail.com>
2024-12-17 23:56:18 -0800

    main project

---
+++ src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+import Card from "@mui/material/Card";
+
+import MDBox from "components/MDBox";
+import MDInput from "components/MDInput";
+import MDButton from "components/MDButton";
+
+import BasicLayout from "layouts/authentication/components/BasicLayout";
+
+import bgImage from "assets/images/logo.png";
+
+function Basic() {
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  return (
+    <BasicLayout>
+      <Card>
+        <MDBox
+          variant="gradient"
+          bgColor="info"
+          borderRadius="lg"
+          coloredShadow="info"
+          mx={2}
+          mt={-3}
+          p={2}
+          mb={1}
+          textAlign="center"
+        >
+          <MDBox
+            component="img"
+            src={bgImage}
+            alt="background"
+            borderRadius="lg"
+            width="60%"
+            height="60"
+            sx={{ objectFit: "cover" }}
+          />
+        </MDBox>
+        <MDBox pt={4} pb={3} px={3}>
+          <MDBox component="form" role="form">
+            <MDBox mb={2}>
+              <MDInput type="email" label="Email" fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="password" label="Password" fullWidth />
+            </MDBox>
+            <MDBox mt={4} mb={1}>
+              <MDButton variant="gradient" color="info" fullWidth>
+                sign in
+              </MDButton>
+            </MDBox>
+          </MDBox>
+        </MDBox>
+      </Card>
+    </BasicLayout>
+  );
+}
+
+export default Basic;
